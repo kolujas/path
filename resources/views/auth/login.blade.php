257763d2e76d8bfa7d96d9d9b29e6937996dfce4
kolujAs <fernandodeibe@gmail.com>
2020-09-14 12:57:42 -0300

    Login e importacion de background.css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,8 +8,26 @@
     Log In Path
 @endsection
 
-@section('main')
-
+@section('main')    
+    <section class="centrar-form mx-auto">        
+        <form class="col-12" method="post">
+            <div class="text-center mb-5">
+                <h2 class="h2">Ingresá para comenzar</h2>
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input id="email" type="email" name="correo" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="pass">Contraseña</label>
+                <input id="pass" type="password" name="clave" class="form-control">
+            </div>
+            <div class="d-flex justify-content-center align-items-center">
+                <button type="submit" class="btn background background-one text-white">Ingresar</button>
+            </div>
+            
+        </form>
+    </section>
 @endsection
 
 @section('js')
