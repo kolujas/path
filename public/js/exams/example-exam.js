1cd4faaa8cda84ef29c45c059eadbe5e4c2c8bf4
kolujAs <fernandodeibe@gmail.com>
2020-10-22 20:21:14 -0300

    copy-paste y tooltip

--- public/js/exams/example-exam.js
+++ public/js/exams/example-exam.js
@@ -309,3 +309,70 @@ $(document).mouseleave(function () {
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();   
 });
+
+
+(function () {
+    var onload = window.onload;
+
+    window.onload = function () {
+        if (typeof onload == "function") {
+            onload.apply(this, arguments);
+        }
+
+        var fields = [];
+        var inputs = document.getElementsByTagName("input");
+        var textareas = document.getElementsByTagName("textarea");
+
+        for (var i = 0; i < inputs.length; i++) {
+            fields.push(inputs[i]);
+        }
+
+        for (var i = 0; i < textareas.length; i++) {
+            fields.push(textareas[i]);
+        }
+
+        for (var i = 0; i < fields.length; i++) {
+            var field = fields[i];
+
+            if (typeof field.onpaste != "function" && !!field.getAttribute("onpaste")) {
+                field.onpaste = eval("(function () { " + field.getAttribute("onpaste") + " })");
+            }
+
+            if (typeof field.onpaste == "function") {
+                var oninput = field.oninput;
+
+                field.oninput = function () {
+                    if (typeof oninput == "function") {
+                        oninput.apply(this, arguments);
+                    }
+
+                    if (typeof this.previousValue == "undefined") {
+                        this.previousValue = this.value;
+                    }
+
+                    var pasted = (Math.abs(this.previousValue.length - this.value.length) > 1 && this.value != "");
+
+                    if (pasted && !this.onpaste.apply(this, arguments)) {
+                        this.value = this.previousValue;
+                    }
+
+                    this.previousValue = this.value;
+                };
+
+                if (field.addEventListener) {
+                    field.addEventListener("input", field.oninput, false);
+                } else if (field.attachEvent) {
+                    field.attachEvent("oninput", field.oninput);
+                }
+            }
+        }
+    }
+})();
+
+$('body').on('copy paste', 'input', function (e)
+    { e.preventDefault();
+ });
+
+$('body').on('copy paste', 'textarea', function (e)
+    { e.preventDefault();
+ });
