b310f39ccac2d1113051ac86cc0b9abbe6ba7bad
kolujAs <fernandodeibe@gmail.com>
2020-10-23 18:05:01 -0300

    strikes y confirmation

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,13 @@
      */
     Route::get('/exam/{id_exam}/finished', 'ExamController@finished')->name('exam.finished');
     Route::middleware('auth.guards')->group(function(){
-        Route::middleware(['ended', 'student', 'status'])->group(function(){
+        // Route::middleware(['ended', 'student', 'status'])->group(function(){
             Route::get('/exam/{id_exam}/rules', 'ExamController@rules')->name('exam.rules');
             // Route::middleware(['scheduled_date_time', 'confirmed'])->group(function(){
                 Route::post('/auth/exam/{id_exam}', 'ExamController@auth')->name('exam.auth');
                 Route::get('/exam/{id_exam}', 'ExamController@show')->name('exam.show');
         //     });
-        });
+        // });
         Route::middleware('admin')->group(function(){
             Route::post('/exams/create', 'ExamController@doCreate')->name('exam.doCreate');
             Route::put('/exams/{id_exam}/edit', 'ExamController@doEdit')->name('exam.doEdit');
