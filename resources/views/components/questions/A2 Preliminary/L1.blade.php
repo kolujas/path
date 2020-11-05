67099fd1c202070c6c492f47e96e95c3df9037b8
Archimak <43325751+JuanCruzAGB@users.noreply.github.com>
2020-11-05 20:23:36 -0300

    A2 Preliminary started

--- resources/views/components/questions/A2 Preliminary/L1.blade.php
+++ resources/views/components/questions/A2 Preliminary/L1.blade.php
@@ -1,11 +1,156 @@
-<section id="dropdown-TIPO-NUMERO" class="question dropdown dropdown-click closed col-12 col-lg-10 col-xl-8 mb-4">
+<section id="dropdown-listening-1" class="question dropdown dropdown-click closed col-12 col-lg-10 col-xl-8 mb-4">
     <a href="#" class="dropdown-header">
-        <span>CONSIGNA</span>
+        <span>1. Listen to comparasions between 3 groups of pictures: A, B and C, then choose the right name or number.<mark>One has been done for you</mark>.</span>
         <button class="dropdown-button text-right">
             <i class="dropdown-icon fas fa-chevron-down"></i>
         </button>
     </a>
-    <main class="dropdown-menu-content NUMBER_NAME-question-container px-xl-4">
-        EJERCICIO
+    <main class="dropdown-menu-content one-question-container px-xl-4">
+        <div class="d-flex justify-content-center my-4">
+            <audio class="audio d-none" controls controlsList="nodownload">
+                <source
+                    src="https://upload.wikimedia.org/wikipedia/en/9/9f/Sample_of_%22Another_Day_in_Paradise%22.ogg"
+                    type="audio/ogg">
+                Your browser does not supports the audio.
+            </audio>
+            <div class="audio-div">
+                <i class="fas fa-play play-icon"></i><button class="audioBtn" type="button">Play Audio</button>
+            </div>
+        </div>
+
+        <div class="row justify-content-md-between justify-content-lg-center py-4">
+            <div class="col-12 col-md-6 col-lg-3 mb-4 mb-lg-0 card-min-width">
+                <div class="card">
+                    <img src="../../img/recursos/writing-1-1.jpg" class="img-fluid" alt="image-one-question-one">
+                    <div class="card-body mx-auto p-0">
+                        <ol class="answer-words d-flex justify-content-around justify-content-lg-center">
+                            <li class="answers inline mx-lg-2"><label><input type="radio" name="A2_Preliminary:L1[1]" value="Ben">Ben</label></li>
+                            <li class="inline mx-lg-2">/</li>
+                            <li class="answers inline mx-lg-2"><label><input type="radio" name="A2_Preliminary:L1[1]" value="Sid">Sid</label></li>
+                            <li class="inline mx-lg-2">/</li>
+                            <li class="answers inline mx-lg-2"><label><input type="radio" name="A2_Preliminary:L1[1]" value="Tom">Tom</label></li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3 mb-4 mb-lg-0 card-min-width">
+                <div class="card">
+                    <img src="../../img/recursos/writing-1-1.jpg" class="img-fluid" alt="image-one-question-one">
+                    <div class="card-body mx-auto p-0">
+                        <ol class="answer-words d-flex justify-content-around justify-content-lg-center">
+                            <li class="answers inline mx-lg-2"><label><input type="radio" name="A2_Preliminary:L1[1]" value="Ben">Ben</label></li>
+                            <li class="inline mx-lg-2">/</li>
+                            <li class="answers inline mx-lg-2"><label><input type="radio" name="A2_Preliminary:L1[1]" value="Sid">Sid</label></li>
+                            <li class="inline mx-lg-2">/</li>
+                            <li class="answers inline mx-lg-2"><label><input type="radio" name="A2_Preliminary:L1[1]" value="Tom">Tom</label></li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3 mb-4 mb-lg-0 card-min-width">
+                <div class="card">
+                    <img src="../../img/recursos/writing-1-1.jpg" class="img-fluid" alt="image-one-question-one">
+                    <div class="card-body mx-auto p-0">
+                        <ol class="answer-words d-flex justify-content-around justify-content-lg-center">
+                            <li class="answers inline mx-lg-2"><label><input type="radio" name="A2_Preliminary:L1[1]" value="Ben">Ben</label></li>
+                            <li class="inline mx-lg-2">/</li>
+                            <li class="answers inline mx-lg-2"><label><input type="radio" name="A2_Preliminary:L1[1]" value="Sid">Sid</label></li>
+                            <li class="inline mx-lg-2">/</li>
+                            <li class="answers inline mx-lg-2"><label><input type="radio" name="A2_Preliminary:L1[1]" value="Tom">Tom</label></li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row justify-content-md-between justify-content-lg-center py-4">
+            <div class="col-12 col-md-6 col-lg-3 mb-4 mb-lg-0 card-min-width">
+                <div class="card">
+                    <img src="../../img/recursos/writing-1-1.jpg" class="img-fluid" alt="image-one-question-one">
+                    <div class="card-body mx-auto p-0">
+                        <ol class="answer-words d-flex justify-content-around justify-content-lg-center">
+                            <li class="answers inline mx-lg-2"><label><input type="radio" name="A2_Preliminary:L1[1]" value="56">56</label></li>
+                            <li class="inline mx-lg-2">/</li>
+                            <li class="answers inline mx-lg-2"><label><input type="radio" name="A2_Preliminary:L1[1]" value="68">68</label></li>
+                            <li class="inline mx-lg-2">/</li>
+                            <li class="answers inline mx-lg-2"><label><input type="radio" name="A2_Preliminary:L1[1]" value="21">21</label></li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3 mb-4 mb-lg-0 card-min-width">
+                <div class="card">
+                    <img src="../../img/recursos/writing-1-1.jpg" class="img-fluid" alt="image-one-question-one">
+                    <div class="card-body mx-auto p-0">
+                        <ol class="answer-words d-flex justify-content-around justify-content-lg-center">
+                            <li class="answers inline mx-lg-2"><label><input type="radio" name="A2_Preliminary:L1[1]" value="56">56</label></li>
+                            <li class="inline mx-lg-2">/</li>
+                            <li class="answers inline mx-lg-2"><label><input type="radio" name="A2_Preliminary:L1[1]" value="68">68</label></li>
+                            <li class="inline mx-lg-2">/</li>
+                            <li class="answers inline mx-lg-2"><label><input type="radio" name="A2_Preliminary:L1[1]" value="21">21</label></li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3 mb-4 mb-lg-0 card-min-width">
+                <div class="card">
+                    <img src="../../img/recursos/writing-1-1.jpg" class="img-fluid" alt="image-one-question-one">
+                    <div class="card-body mx-auto p-0">
+                        <ol class="answer-words d-flex justify-content-around justify-content-lg-center">
+                            <li class="answers inline mx-lg-2"><label><input type="radio" name="A2_Preliminary:L1[1]" value="56">56</label></li>
+                            <li class="inline mx-lg-2">/</li>
+                            <li class="answers inline mx-lg-2"><label><input type="radio" name="A2_Preliminary:L1[1]" value="68">68</label></li>
+                            <li class="inline mx-lg-2">/</li>
+                            <li class="answers inline mx-lg-2"><label><input type="radio" name="A2_Preliminary:L1[1]" value="21">21</label></li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row justify-content-md-between justify-content-lg-center py-4">
+            <div class="col-12 col-md-6 col-lg-3 mb-4 mb-lg-0 card-min-width">
+                <div class="card">
+                    <img src="../../img/recursos/writing-1-1.jpg" class="img-fluid" alt="image-one-question-one">
+                    <div class="card-body mx-auto p-0">
+                        <ol class="answer-words d-flex justify-content-around justify-content-lg-center">
+                            <li class="answers inline mx-lg-2"><label><input type="radio" name="A2_Preliminary:L1[1]" value="2A">2A</label></li>
+                            <li class="inline mx-lg-2">/</li>
+                            <li class="answers inline mx-lg-2"><label><input type="radio" name="A2_Preliminary:L1[1]" value="5C">5C</label></li>
+                            <li class="inline mx-lg-2">/</li>
+                            <li class="answers inline mx-lg-2"><label><input type="radio" name="A2_Preliminary:L1[1]" value="3B">3B</label></li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3 mb-4 mb-lg-0 card-min-width">
+                <div class="card">
+                    <img src="../../img/recursos/writing-1-1.jpg" class="img-fluid" alt="image-one-question-one">
+                    <div class="card-body mx-auto p-0">
+                        <ol class="answer-words d-flex justify-content-around justify-content-lg-center">
+                            <li class="answers inline mx-lg-2"><label><input type="radio" name="A2_Preliminary:L1[1]" value="2A">2A</label></li>
+                            <li class="inline mx-lg-2">/</li>
+                            <li class="answers inline mx-lg-2"><label><input type="radio" name="A2_Preliminary:L1[1]" value="5C">5C</label></li>
+                            <li class="inline mx-lg-2">/</li>
+                            <li class="answers inline mx-lg-2"><label><input type="radio" name="A2_Preliminary:L1[1]" value="3B">3B</label></li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3 mb-4 mb-lg-0 card-min-width">
+                <div class="card">
+                    <img src="../../img/recursos/writing-1-1.jpg" class="img-fluid" alt="image-one-question-one">
+                    <div class="card-body mx-auto p-0">
+                        <ol class="answer-words d-flex justify-content-around justify-content-lg-center">
+                            <li class="answers inline mx-lg-2"><label><input type="radio" name="A2_Preliminary:L1[1]" value="2A">2A</label></li>
+                            <li class="inline mx-lg-2">/</li>
+                            <li class="answers inline mx-lg-2"><label><input type="radio" name="A2_Preliminary:L1[1]" value="5C">5C</label></li>
+                            <li class="inline mx-lg-2">/</li>
+                            <li class="answers inline mx-lg-2"><label><input type="radio" name="A2_Preliminary:L1[1]" value="3B">3B</label></li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 </section>
--- resources/views/components/questions/A2 Preliminary/L3.blade.php
+++ resources/views/components/questions/A2 Preliminary/L3.blade.php
@@ -1,11 +1,22 @@
-<section id="dropdown-TIPO-NUMERO" class="question dropdown dropdown-click closed col-12 col-lg-10 col-xl-8 mb-4">
+<section id="dropdown-listening-3" class="question dropdown dropdown-click closed col-12 col-lg-10 col-xl-8 mb-4">
     <a href="#" class="dropdown-header">
-        <span>CONSIGNA</span>
+        <span>3. You receive a voice message from a friend telling you about a problem he has. Write to him to give some advice.</span>
         <button class="dropdown-button text-right">
             <i class="dropdown-icon fas fa-chevron-down"></i>
         </button>
     </a>
-    <main class="dropdown-menu-content NUMBER_NAME-question-container px-xl-4">
-        EJERCICIO
+    <main class="dropdown-menu-content three-question-container px-xl-4">
+        <div class="d-flex justify-content-center my-4">
+            <audio class="audio d-none" controls controlsList="nodownload">
+                <source
+                    src="https://upload.wikimedia.org/wikipedia/en/9/9f/Sample_of_%22Another_Day_in_Paradise%22.ogg"
+                    type="audio/ogg">
+                Your browser does not supports the audio.
+            </audio>
+            <div class="audio-div">
+                <i class="fas fa-play play-icon"></i><button class="audioBtn" type="button">Play Audio</button>
+            </div>
+        </div>
+
     </main>
 </section>
