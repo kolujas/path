01a671b45875457df6d7692a89f3c9a1dbcf697d
kolujAs <fernandodeibe@gmail.com>
2020-11-06 09:30:01 -0300

    listening archiever

--- resources/views/components/questions/A1 Achiever/L1.blade.php
+++ resources/views/components/questions/A1 Achiever/L1.blade.php
@@ -1,11 +1,41 @@
-<section id="dropdown-TIPO-NUMERO" class="question dropdown dropdown-click closed col-12 col-lg-10 col-xl-8 mb-4">
+<section id="dropdown-listening-1" class="question dropdown dropdown-click closed col-12 col-lg-10 col-xl-8 mb-4">
     <a href="#" class="dropdown-header">
-        <span>CONSIGNA</span>
+        <span>1. Listen to Jenny and Alan talking about their weekend activities. Match the times to the activities. One time and one activity shown below is not used. <mark>One has been done for you.</mark></span>
         <button class="dropdown-button text-right">
             <i class="dropdown-icon fas fa-chevron-down"></i>
         </button>
     </a>
-    <main class="dropdown-menu-content NUMBER_NAME-question-container px-xl-4">
-        EJERCICIO
+    <main class="dropdown-menu-content one-question-container px-xl-4">
+        <div class="d-flex justify-content-center my-4">
+            <audio class="audio d-none" controls controlsList="nodownload">
+                <source
+                    src="https://upload.wikimedia.org/wikipedia/en/9/9f/Sample_of_%22Another_Day_in_Paradise%22.ogg"
+                    type="audio/ogg">
+                Your browser does not supports the audio.
+            </audio>
+            <div class="audio-div">
+                <i class="fas fa-play play-icon"></i><button class="audioBtn" type="button">Play Audio</button>
+            </div>
+        </div>
+        <div>
+            <ul class="bg-archiever-1 d-flex justify-content-center">
+                <li class="mx-2 disabled">a. 10 a.m.</li>
+                <li class="mx-2">b. 11:30 a.m.</li>
+                <li class="mx-2">c. 2 p.m.</li>
+                <li class="mx-2">d. 5:30 p.m.</li>
+                <li class="mx-2">e. 9 p.m.</li>
+            </ul>
+        </div>
+        <div class="py-4 px-2 px-md-3">
+            <p class="mb-4"> <span class="font-weight-bold">a.</span>  I usually get up at <input class="input input-medium text-center"
+                value="10 a.m" disabled type="text">.</p>
+            <p class="mb-4"> <span class="font-weight-bold">b.</span>  We eat quite late on Saturdays, at around <input class="input input-medium text-center"
+                 disabled type="text">.</p>
+            <p class="mb-4"> <span class="font-weight-bold">c.</span>  We always stop for a coffee at <input class="input input-medium text-center"
+                 disabled type="text"> in the morning..</p>
+            <p class="mb-4"> <span class="font-weight-bold">d.</span> At <input class="input input-medium text-center"
+                 disabled type="text"> in the afternoon we watch a film..</p>
+           
+        </div>
     </main>
 </section>
