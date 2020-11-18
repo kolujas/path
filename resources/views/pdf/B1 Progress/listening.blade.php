0baace2922d550007244a56912639ace3713b553
Archimak <43325751+JuanCruzAGB@users.noreply.github.com>
2020-11-18 15:42:31 -0300

    Nombre del Candidato ocultado en los PDF

--- resources/views/pdf/B1 Progress/listening.blade.php
+++ resources/views/pdf/B1 Progress/listening.blade.php
@@ -11,11 +11,7 @@
 @section('main')
     <header>
         <h2 style="text-align: center; padding-top: 2rem;">{{ $module->folder }} {{ ucfirst($module->file) }}</h2>
-        @if ($candidate->full_name)
-            <h3 style="text-align: center; color: #0091B7;">{{ $candidate->full_name }} ({{ $candidate->candidate_number }})</h3>
-        @else
-            <h3 style="text-align: center; color: #0091B7;">Candidate Number ({{ $candidate->candidate_number }})</h3>
-        @endif
+        <h3 style="text-align: center; color: #0091B7;">Candidate Number: ({{ $candidate->candidate_number }})</h3>
         <p style="text-align: center;">Strikes: {{ $answers['strikes'] }}</p>
     </header>
 
