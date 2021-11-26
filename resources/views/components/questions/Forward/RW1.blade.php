d99051bb2011d9e1f047d08b69f0bbcdb4e34f14
kolujAs <fernandodeibe@gmail.com>
2021-11-26 16:38:32 -0300

    forward writing (falta el 3)

--- resources/views/components/questions/Forward/RW1.blade.php
+++ resources/views/components/questions/Forward/RW1.blade.php
@@ -1,61 +1,37 @@
 <section id="dropdown-forward-writing-1" class="question dropdown dropdown-click closed col-12 col-lg-10 col-xl-8 mb-4">
     <a href="#" class="dropdown-header">
-        <span>1. Read the text, then answer the questions.</span>
+        <span>1. Read the text then answer the questions on the following page.</span>
         <button class="dropdown-button text-right">
             <i class="dropdown-icon fas fa-chevron-down"></i>
         </button>
     </a>
     <main class="dropdown-menu-content one-question-container px-xl-4">
-        <h3 class="title col-12 mb-4 h5"><b>How Your Body Modifies Your Message</b><i> by Max Kelly</i></h3>
-        <img src="" alt="">
+        <h3 class="title col-12 mb-4 h5"><b>The Truth About What’s Really in your Water Supply</b><i> (edited. Source: Aquacure)</i></h3>
         <div class="info mx-auto my-4">
-            <p>Non-verbal communication, often referred to as body language, is a means by which we use our
-                bodies to communicate. This can be by facial expressions, touch, and body and eye movements.
-                When you fold your arms and raise your eyes, for example, you may be expressing disapproval
-                without even saying anything. The interesting thing about non-verbal communication is that it’s often
-                done with no conscious awareness. We might rub our chin, often when we are interested or engaged,
-                without realising that we are doing so. For this reason, non-verbal communication should not be
-                confused with sign language, which is commonly used as a means to communicate with people who
-                have hearing impairments. Sign language uses specific hand and facial movements to communicate
-                specific messages.</p>
-            <p>Unfortunately for some of us, not all non-verbal communication is universal, and some movements are
-                interpreted differently depending on geographical region. For example, the raising of the index and
-                middle fingers, in most cultures, simply means two or twice, but in Great Britain it is an offensive
-                gesture. Do you ever place the lower part of your leg over the thigh of your other leg so that the sole of
-                your foot can be clearly seen? In most cultures this has little or no meaning, but is offensive in Arabic
-                culture, particularly when the sole of the foot is facing the person.
-                </p>
-            <p>Facial expressions, on the other hand, are usually universal. The most common is when we smile.
-                Genuine smiles mean the same to everyone.
-                Law enforcement agencies often use non-verbal communication to assess the guilt of a suspect. Even
-                though the reliability of lie detection techniques has actually been rejected by many academics, this is
-                still common in many countries.</p>
-            <p>Gamblers have to be extremely careful that their facial expression, particularly in their eyes, does not
-                betray their confidence or lack of. Poker players often try to assess the power of their opponent’s hand
-                by scrutinising his or her face. This has led to the expression to have a poker face, meaning to give
-                nothing away by facial expression. Unfortunately for some however, not all facial movement can be
-                controlled, in particular the nervous tic, where facial muscles move spasmodically.</p>
-            <p>The coronavirus pandemic has forced many of us to rethink our use of non-verbal communication.
-                The handshake particularly is now unacceptable to many people. Cultures that use kissing the other’s
-                cheek once, twice, or even three times have also had to reconsider the appropriacy of such gestures.
-                Touching elbow to elbow, or foot to foot, is becoming more commonplace, yet in situations where
-                social distancing is required, there can be no touching. The omission of these methods of
-                communicating pleasure in meeting or seeing someone could lead to societal problems which cannot
-                yet be fully understood. </p>
-                
+            <p>We all take it for granted that the water coming out of our taps has been treated to a high level. Yet drinking water quality varies greatly by region and impurities can enter your supply even from the individual pipes leading to your home.</p>
+            <p> We examine what’s really in the tap water we drink every day.</p>
+            <h3 class="h5"><b>Chlorine</b></h3>
+            <p> Chlorine is used in the water treatment process to kill harmful bacteria and to keep water germ-free when on its journey to your taps. While chlorination is a tried and tested method of disinfecting mains water supplies, it can potentially react with certain naturally occurring organic compounds, producing by-products such as chloroform. Regular monitoring of these compounds in municipal water supplies is, however, required by law so the chances of consuming any harmful substances is extremely small. Whilst the health risks related to chlorine are minimal (and certainly less than the risks posed by inadequate disinfection according to the World Health Organisation) it can impart a strong chemical taste and smell, which can be unpleasant, particularly to those with sensitive palates. 
+            </p>
+            <h3 class="h5"><b>Limescale</b></h3>
+            <p>Limescale is the chalky deposit made up of calcium and magnesium carbonate formed when hard water (i.e.
+                water with high dissolved mineral content) is heated. Water hardness varies greatly by region, with hard water areas seeing greater issues with limescale build up on kettles, coffee machines and taps. Not only does limescale look unsightly but it also affects the efficiency of heating elements in kettles and boilers. The Carbon Trust state that just 1mm of scale will require “a 7% increase in energy input to the boiler to meet the same heat demand”.</p>
+            <h3 class="h5"><b>Fluoride</b></h3>    
+            <p>Most water supplies naturally contain fluoride at varying levels depending on the source while lead can also find its way into your water though this depends on the presence of old lead water mains in the supply network.
+            Fluoride can occur naturally and there are maximum levels allowed in mains water, though in some
+            regions fluoride is added to municipal water supplies. Artificial fluoridation of mains water has long been a
+            controversial issue with both advocates and critics.
+            Fluoride is medically proven to reduce tooth decay in children and since tooth decay is statistically one of
+            the most widespread chronic diseases in cities, fluoridation schemes have been in place to add it to mains water for around 60 years. But even today, it’s controversial with critics arguing that high levels of fluoride can lead to dental and skeletal fluorosis (discolouration and damage to teeth and bones).</p>
+            <h3 class="h5"><b>Microplastics</b></h3> 
+            <p>Plastic microfibres have been found to contaminate 72% of tap water samples in European nations, including the
+            UK. These microfibres are be believed to come from various sources including clothes made of synthetic fabrics, car tyres, paint and microbeads from cosmetics. To give an idea of the scale of microplastics pollution, a fleece jacket can shed as many as 250,000 microfibres in a single wash. The health risks are still unclear, though microplastics are known to contain and absorb toxic chemicals. Research has also indicated that conditions inside the digestive systems of wild animals could potentially be facilitating the release of those toxins. More research is currently needed to determine the exact effect on human health. </p>
         </div>
 
-        <div class="complete col-12 col-lg-10 col-xl-8 mx-auto my-4 position-relative">
-            <label for="B2_ex1-1" class="indent">a. What does the article say about non-verbal communication in different cultures?</label>
-            <textarea id="B2_ex1-1" name="Forward:RW1[1]" class="px-2 text"></textarea>
-        </div>
-        <div class="complete col-12 col-lg-10 col-xl-8 mx-auto my-4 position-relative">
-            <label for="B2_ex1-2" class="indent">b. According to the article, in which situations would a person need to be aware of their non-verbal communication?</label>
-            <textarea id="B2_ex1-2" name="Forward:RW1[2]" class="px-2 indent-two text"></textarea>
-        </div>
-        <div class="complete col-12 col-lg-10 col-xl-8 mx-auto my-4 position-relative">
-            <label for="B2_ex1-3" class="indent">c. How is the coronavirus pandemic affecting non-verbal communication in your country? Give examples.</label>
-            <textarea id="B2_ex1-3" name="Forward:RW1[3]" class="px-2 indent-two text"></textarea>
-        </div>
+            <div class="row d-flex justify-content-center ml-1 py-4 px-2 px-md-3 complete">
+                <p class="complete col-12 pt-4 mb-4"><b>1a.</b> Of the components described, which are sometimes added by water authorities? Do you agree with these additions? Explain your answer. </p><textarea name="Forward:RW2[1]" class="input input-medium text-left w-75 pl-2"></textarea>
+                <p class="complete col-12 pt-4 mb-4"><b>1b.</b> Y Of the components described, which are unintended? What solutions would you suggest?</p><textarea name="Forward:RW2[2]" class="input input-medium text-left w-75"></textarea>
+                <p class="complete col-12 pt-4 mb-4"><b>1c.</b> . How does the article make you feel about drinking a. tap water and b. bottled water? </p><textarea name="Forward:RW2[3]" class="input input-medium text-left w-75"></textarea>
+            </div> 
     </main>
 </section>
